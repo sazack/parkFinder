ef5a93091c74da9c3309071c4145c9a9610e76d0
Sajak Upadhyaya <sajak1994@gmail.com>
2021-02-06 14:50:25 -0500

    adding dist to deploy it on aws

--- server/index.ts
+++ server/index.ts
@@ -10,11 +10,16 @@ class NodeApp {
     this.Express = express();
     const port = '8088';
     this.Express.use(urlencoded({extended: false}));
+    this.Express.use(express.static(process.cwd()+"/dist/parkFinder/"));
     this.server = http.createServer(this.Express);
     this.server.listen(port, async () => {
       console.log('Node Server Listening on Port: ' + port);
     });
     this.Express.use('/api', myRouter );
+
+    this.Express.use('/', (req,res) => {
+      res.sendFile(process.cwd()+"/dist/parkFinder/index.html")
+    });
   }
 }
 
